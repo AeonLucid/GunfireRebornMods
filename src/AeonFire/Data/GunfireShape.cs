4957914b5fad593a36181aa7c86b5310ff307ade
WhitePhant0m <kallunki.aleksi@gmail.com>
2022-12-07 05:57:39 +0200

    Updated to include XP drops from new gamemode.

--- src/AeonFire/Data/GunfireShape.cs
+++ src/AeonFire/Data/GunfireShape.cs
@@ -2,17 +2,21 @@
 
 internal static class GunfireShape
 {
-    public const int SMALL = 4502;
-    public const int MEDIUM = 4503;
-    public const int SPECIAL = 4504;
-    public const int Grenade = 4508;
-    public const int RelifeItem = 5512;
-    public const int HPItem = 5513;
-    public const int Crustal = 5511;
-    
-    public const int DropBullet = 5501;
-    public const int DropCoin = 5502;
-    public const int DropRelic = 5503;
-    public const int DropMedic = 5504;
-    public const int DropKey = 5505;
+    public const int SMALL = 4502; // Normal Ammo
+    public const int MEDIUM = 4503; // Large Ammo
+    public const int SPECIAL = 4504; // Special Ammo
+    public const int Grenade = 4508; // Secendary
+    public const int RelifeItem = 5512; // ???
+    public const int HPItem = 5513; // Bun
+    public const int Crustal = 5511; // ???
+
+    public const int DropBullet = 5501; // ???
+    public const int DropCoin = 5502; // Copper
+    public const int DropRelic = 5503; // ???
+    public const int DropMedic = 5504; // Soul Essence
+    public const int DropKey = 5505; // ???
+
+    public const int DropXPSmall = 5516; // Spiritual Assault XP (blue)
+    public const int DropXPMedium = 5541; // Spiritual Assault XP (pink)
+    public const int DropXPLarge = 5542; // Spiritual Assault XP (orange)
 }
